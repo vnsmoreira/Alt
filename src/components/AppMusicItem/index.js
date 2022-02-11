5e9f27f683a48c81c1d8c7f53eb4f74ac113406f
Vinícius <vnsmoreira.me@gmail.com>
2022-02-11 20:58:12 -0300

    Create download progress states and conditional render

--- src/components/AppMusicItem/index.js
+++ src/components/AppMusicItem/index.js
@@ -1,67 +1,150 @@
 import { useState, useContext, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
-import { AntDesign, SimpleLineIcons } from '@expo/vector-icons';
+import { AntDesign, SimpleLineIcons, MaterialIcons } from '@expo/vector-icons';
 import ProgressCircle from 'react-native-progress-circle';
 import { downloadFile } from '../../modules/download';
 import { PlayerContext } from '../../contexts/player';
-import { formatTitle } from './utils';
+import { StorageContext } from '../../contexts/storage';
+import { baseURL, downloadEndpoint } from '../../services/apis/index.js';
 
 const MusicItem = ({ item }) => {
   const [downloadProgress, setDownloadProgress] = useState(0);
+  const [isDownloading, setIsDownloading] = useState(downloadProgress != 0);
+  const [isDownloaded, setIsDownloaded] = useState(false);
+  const [isPlayAudioFunctionBeingExecuted, setIsPlayAudioFunctionBeingExecuted] = useState(false);
+  const [uri, setUri] = useState('');
+  const { player, loaded, loading, playing, currentAudioId } = useContext(PlayerContext);
+  const AsyncStorage = useContext(StorageContext);
 
-  const title = formatTitle(item);
   const id = item.id;
+  const title = item.title;
+  const duration = item.duration;
+  const author = item.author;
+  const thumbnailUri = item.thumbnailUri;
 
-  const { player, playerState } = useContext(PlayerContext);
+  const isAudioSelected = currentAudioId == id;
+  const isAudioLoading = isAudioSelected && loading;
+  const isAudioPlaying = isAudioSelected && playing;
 
-  const pressHandler = async () => {
-    player.playMusic({
+  useEffect(async () => {
+    const remoteUri = baseURL + downloadEndpoint + item.id;
+    let localUri = '';
+
+    try {
+      const { exists } = await AsyncStorage.checkAudio(item.id);
+      if (exists) {
+        const currentAudio = AsyncStorage.storage[item.id];
+
+        localUri = currentAudio.uri;
+        setIsDownloaded(true);
+      }
+
+      localUri ? setUri(localUri) : setUri(remoteUri);
+    } catch (error) {
+      console.log('Error while getting storage status');
+    }
+  }, []);
+
+  const titleStyle = isAudioPlaying ? '#ec73ff' : isAudioSelected ? '#6a007a' : 'white';
+
+  const getAudioInfo = (item, uri) => {
+    const info = {
       id,
-      title,
-      author: item.author.name,
-      thumbnailUri: item.bestThumbnail.url,
-    });
+      title: title,
+      duration: item.duration,
+      author: item.author,
+      thumbnailUri: item.thumbnailUri,
+      uri,
+    };
+
+    return JSON.parse(JSON.stringify(info));
   };
 
-  const downloadHandler = async () => {
-    await downloadFile(id, item.title, setDownloadProgress);
+  const handlePlayAudio = async () => {
+    if (isPlayAudioFunctionBeingExecuted) return;
+
+    setIsPlayAudioFunctionBeingExecuted(true);
+    const audioInfo = getAudioInfo(item, uri);
+
+    if (loaded) {
+      if (isAudioSelected) {
+        await player.replayAudio();
+      } else {
+        await player.unloadAudio();
+        await player.loadAudio(audioInfo);
+        await player.playAudio();
+      }
+    } else {
+      await player.loadAudio(audioInfo);
+      await player.playAudio();
+    }
+
+    setIsPlayAudioFunctionBeingExecuted(false);
   };
+
+  const handleDownloadAudio = async () => {
+    if (isDownloaded || isDownloading) return;
+
+    try {
+      const { saved, audioLocalUri } = await downloadFile(id, setDownloadProgress, setIsDownloaded);
+
+      if (saved) {
+        const audioInfo = getAudioInfo(item, audioLocalUri);
+        await AsyncStorage.saveAudio(id, audioInfo);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteAudio = async () => {};
 
   return (
     <View style={styles.musicItem}>
-      <TouchableOpacity style={styles.presentationContainer} onPress={pressHandler}>
+      <TouchableOpacity style={styles.presentationContainer} onPress={handlePlayAudio}>
         <View style={styles.thumbnailContainer}>
-          <Image style={styles.thumbnail} source={{ uri: item.bestThumbnail.url }} />
+          <Image style={styles.thumbnail} source={{ uri: thumbnailUri }} />
         </View>
 
         <View style={styles.aboutContainer}>
-          <Text numberOfLines={1} style={styles.musicTitle}>
+          <Text
+            numberOfLines={1}
+            style={{
+              ...styles.musicTitle,
+              ...{ color: titleStyle },
+            }}
+          >
             {title}
           </Text>
           <View style={styles.musicInfo}>
-            <Text style={styles.musicDuration}>{item.duration}</Text>
-            <Text style={styles.musicAuthor}>{item.author.name}</Text>
+            <Text style={styles.musicDuration}>{duration}</Text>
+            <Text numberOfLines={1} style={styles.musicAuthor}>
+              {author}
+            </Text>
           </View>
         </View>
       </TouchableOpacity>
 
-      {/* componentizar musicDownloadButton */}
       <View style={styles.optionsContainer}>
-        <TouchableOpacity onPress={downloadHandler}>
+        <TouchableOpacity onPress={handleDownloadAudio}>
           <ProgressCircle
             percent={downloadProgress}
             radius={14}
             borderWidth={2}
             color="#ec73ff"
-            shadowColor="#6a007a"
+            shadowColor={isDownloaded ? '#ec73ff' : '#6a007a'}
             bgColor="#141414"
           >
-            {/* <AntDesign name="close" size={14} color="#ec73ff" /> */}
-            <AntDesign name="arrowdown" size={14} color="#ec73ff" />
+            {isDownloaded ? (
+              <MaterialIcons name="file-download-done" size={14} color="#ec73ff" />
+            ) : isDownloading ? (
+              <AntDesign name="close" size={14} color="#ec73ff" />
+            ) : (
+              <AntDesign name="arrowdown" size={14} color="#ec73ff" />
+            )}
           </ProgressCircle>
         </TouchableOpacity>
-
         <SimpleLineIcons
           style={{ marginLeft: 20, marginRight: 10 }}
           name="options-vertical"
