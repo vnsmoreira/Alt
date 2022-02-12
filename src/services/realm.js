9f9b0e9cc2373471b11cf9da5c2edece29815993
Vinícius <vnsmoreira.me@gmail.com>
2022-02-12 20:05:02 -0300

    Create: realm instance and export basic functins

---
+++ src/services/realm.js
@@ -0,0 +1,35 @@
+import Realm from 'realm';
+import AudioSchema from '../schemas/AudioSchema';
+
+export default function getRealm() {
+  return Realm.open({
+    schema: [AudioSchema],
+    deleteRealmIfMigrationNeeded: true,
+  });
+}
+
+export const deleteAudio = async id => {
+  const realm = await getRealm();
+  const audio = realm.objectForPrimaryKey('Audio', id);
+
+  realm.write(() => {
+    realm.delete(audio);
+  });
+};
+
+export const saveAudio = async audio => {
+  const realm = await getRealm();
+
+  realm.write(() => {
+    realm.create('Audio', audio);
+  });
+};
+
+export const deleteAudioCollection = async () => {
+  const realm = await getRealm();
+  const audioCollection = realm.objects('Audio');
+
+  realm.write(() => {
+    realm.delete(audioCollection);
+  });
+};
