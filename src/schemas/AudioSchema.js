0b56bddeec301980102c332b1b21eab8245002f5
Vinícius <vnsmoreira.me@gmail.com>
2022-02-12 20:04:17 -0300

    Create: realm AudioSchema

---
+++ src/schemas/AudioSchema.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'Audio',
+  properties: {
+    id: 'string',
+    title: 'string',
+    duration: 'string',
+    author: 'string',
+    thumbnailUri: 'string',
+    uri: 'string',
+  },
+};
