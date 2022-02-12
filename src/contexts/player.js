d04f1fc8e95c4821ec2a26156e1d8637574d840f
Vinícius <vnsmoreira.me@gmail.com>
2022-02-11 21:06:50 -0300

    Update: create and provide player methods and status states

--- src/contexts/player.js
+++ src/contexts/player.js
@@ -1,75 +1,187 @@
-import { createContext, useState } from 'react';
+import { createContext, useRef, useState } from 'react';
 import { Audio } from 'expo-av';
 
 export const PlayerContext = createContext({});
 
 export const PlayerProvider = props => {
-  const [musicPlaylist, setMusicPlaylist] = useState([]);
-  const [playerState, setPlayerState] = useState({
-    playlistReference: [],
-    currentIndex: '',
-    isActive: false,
-    isPlaying: false,
-    isLoading: false,
-    positionMs: 0,
-    durationMs: 0,
-    song: {},
-  });
+  const [currentAudioInfo, setCurrentAudioInfo] = useState({});
+  const [currentAudioId, setCurrentAudioId] = useState('');
+  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [looping, setLooping] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [progressPercentage, setProgressPercentage] = useState(0);
+  const sound = useRef(new Audio.Sound());
 
   (async () => {
-    await Audio.setAudioModeAsync({ playsInSilentModeIOS: true, staysActiveInBackground: true });
+    await Audio.setAudioModeAsync({
+      staysActiveInBackground: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      shouldDuckAndroid: false,
+      playThroughEarpieceAndroid: false,
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+    });
   })();
 
-  const [playbackObject, setPlaybackObject] = useState(new Audio.Sound());
-
-  playbackObject.setOnPlaybackStatusUpdate(status => {
-    if (status.isLoaded) {
-      const { isLoaded, isPlaying, isBuffering, positionMillis, durationMillis } = status;
-
-      setPlayerState(playerState => {
-        return {
-          ...playerState,
-          isActive: isLoaded,
-          isPlaying: isPlaying,
-          isLoading: isBuffering,
-          positionMs: positionMillis,
-          durationMs: durationMillis,
-        };
-      });
-    }
-  });
-
-  /*  */
-  const playback = {};
-
-  playback.loadMusic = async () => {};
-
-  /*  */
   const player = {};
 
-  player.playMusic = async musicObj => {
-    const { id, title, author, thumbnailUri } = musicObj;
-    const uri = `http://192.168.15.127:3000/download/${id}`;
-
-    await player.stopMusic();
-    await playbackObject.loadAsync({ uri }, {}, false);
-    await playbackObject.playAsync();
+  player.seekUpdate = async data => {
+    try {
+      const checkLoading = await sound.current.getStatusAsync();
+      if (checkLoading.isLoaded === true) {
+        const result = (data / 100) * duration;
+        await sound.current.setPositionAsync(Math.round(result));
+      }
+    } catch (error) {
+      console.log('Error');
+    }
   };
 
-  player.pauseMusic = async () => {
-    playerState.isPlaying && playerState.isActive ? await playbackObject.pauseAsync() : '';
+  player.stopAudio = async () => {
+    try {
+      const { isLoaded } = await sound.current.getStatusAsync();
+
+      if (isLoaded) {
+        setProgressPercentage(0);
+        setPlaying(false);
+        await sound.current.stopAsync();
+      }
+    } catch (error) {
+      console.log('Error');
+    }
   };
 
-  player.resumeMusic = async () => {
-    !playerState.isPlaying && playerState.isActive ? await playbackObject.playAsync() : '';
+  player.playAudio = async () => {
+    try {
+      const { isLoaded, isPlaying } = await sound.current.getStatusAsync();
+
+      if (isLoaded) {
+        if (!isPlaying) {
+          sound.current.playAsync();
+          setPlaying(true);
+        }
+      }
+    } catch (error) {
+      setPlaying(false);
+    }
   };
 
-  player.stopMusic = async () => {
-    await playbackObject.unloadAsync();
+  player.pauseAudio = async () => {
+    try {
+      const { isLoaded, isPlaying } = await sound.current.getStatusAsync();
+
+      if (isLoaded) {
+        if (isPlaying) {
+          await sound.current.pauseAsync();
+          setPlaying(false);
+        }
+      }
+    } catch (error) {
+      setPlaying(true);
+    }
+  };
+
+  player.replayAudio = async () => {
+    const { isLoaded } = await sound.current.getStatusAsync();
+
+    isLoaded && (await sound.current.playFromPositionAsync(0));
+  };
+
+  player.toggleLooping = async () => {
+    try {
+      const { isLoaded } = await sound.current.getStatusAsync();
+
+      if (!isLoaded) return;
+      const toggledValue = !looping;
+
+      setLooping(toggledValue);
+      await sound.current.setStatusAsync({ isLooping: toggledValue });
+    } catch (error) {
+      setLooping(!looping);
+      console.log('error on toggleLooping');
+    }
+  };
+
+  player.unloadAudio = async () => {
+    await sound.current.unloadAsync();
+    setCurrentAudioInfo({});
+    setCurrentAudioId('');
+    setLoaded(false);
+    setLoading(false);
+    setPlaying(false);
+    setDuration(0);
+    setProgressPercentage(0);
+  };
+
+  player.loadAudio = async audioInfo => {
+    const { id, uri } = audioInfo;
+
+    setCurrentAudioInfo(audioInfo);
+    setCurrentAudioId(id);
+    setLoading(true);
+
+    const checkLoading = await sound.current.getStatusAsync();
+    if (checkLoading.isLoaded === false) {
+      try {
+        const result = await sound.current.loadAsync({ uri }, {}, true);
+        if (result.isLoaded === false) {
+          setCurrentAudioInfo({});
+          setCurrentAudioId('');
+          setLoading(false);
+          setLoaded(false);
+          console.log('Error in loading audio');
+        } else {
+          sound.current.setOnPlaybackStatusUpdate(_onPlaybackStatusUpdate);
+          setLoading(false);
+          setLoaded(true);
+          setDuration(result.durationMillis);
+        }
+      } catch (error) {
+        setCurrentAudioInfo({});
+        setCurrentAudioId('');
+        setLoading(false);
+        setLoaded(false);
+      }
+    } else {
+      setLoading(false);
+      setLoaded(true);
+    }
+  };
+
+  const _onPlaybackStatusUpdate = status => {
+    try {
+      if (status.didJustFinish) {
+        if (status.isLooping) {
+          player.replayAudio();
+        } else {
+          player.stopAudio();
+        }
+      } else if (status.positionMillis) {
+        if (status.durationMillis) {
+          setProgressPercentage((status.positionMillis / status.durationMillis) * 100);
+        }
+      }
+    } catch (error) {
+      console.log('Error on playback status update');
+    }
   };
 
   return (
-    <PlayerContext.Provider value={{ player, playerState }}>
+    <PlayerContext.Provider
+      value={{
+        player,
+        loaded,
+        loading,
+        playing,
+        looping,
+        currentAudioId,
+        currentAudioInfo,
+        progressPercentage,
+      }}
+    >
       {props.children}
     </PlayerContext.Provider>
   );
