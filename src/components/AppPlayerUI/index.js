c09018b035f03642fbcc73a70bed54c4614bd59a
Vinícius <vnsmoreira.me@gmail.com>
2022-02-11 21:03:16 -0300

    Create: player ui style and import player functions

--- src/components/AppPlayerUI/index.js
+++ src/components/AppPlayerUI/index.js
@@ -1,25 +1,46 @@
-import { useContext } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { useContext, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { PlayerContext } from '../../contexts/player';
 
 import styles from './styles';
 
 const PlayerUI = () => {
-  const { playerState, player } = useContext(PlayerContext);
-  const { song, isPlaying } = playerState;
+  const { player, loading, playing, looping, progressPercentage, currentAudioInfo } =
+    useContext(PlayerContext);
 
   return (
     <View style={styles.player}>
-       {playerState.isPlaying ? (
-        <TouchableOpacity onPress={player.pauseMusic} delayPressIn={0}>
-          <Ionicons name="pause" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity onPress={player.resumeMusic}>
-          <FontAwesome name="play" size={24} color="white" />
-        </TouchableOpacity>
-      )}
+      <View style={styles.progressBar}>
+        <View style={{ ...styles.progress, width: `${progressPercentage}%` }}></View>
+      </View>
+      <View style={styles.playerWrapper}>
+        <View style={styles.thumbnailContainer}>
+          <Image style={styles.thumbnail} source={{ uri: currentAudioInfo.thumbnailUri }} />
+        </View>
+        <View style={styles.aboutContainer}>
+          <Text numberOfLines={1} style={styles.musicTitle}>
+            {currentAudioInfo.title}
+          </Text>
+          <View style={styles.musicInfo}>
+            <Text style={styles.musicAuthor}>{currentAudioInfo.author}</Text>
+          </View>
+        </View>
+        <View style={styles.actions}>
+          {playing ? (
+            <TouchableOpacity style={styles.pauseIcon} onPress={player.pauseAudio}>
+              <Ionicons name="pause" size={24} color="white" />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.playIcon} onPress={player.playAudio}>
+              <FontAwesome name="play" size={24} color="white" />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity style={styles.loopIcon} onPress={player.toggleLooping}>
+            <MaterialIcons name="replay" size={24} color={looping ? 'purple' : 'white'} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/AppPlayerUI/styles.js
+++ src/components/AppPlayerUI/styles.js
@@ -6,18 +6,94 @@ const styles = StyleSheet.create({
     height: 60,
 
     marginHorizontal: '1%',
-    marginBottom: 2,
     borderRadius: 5,
 
     backgroundColor: '#212121',
 
-    flexDirection:'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-
     position: 'absolute',
     bottom: 60,
     left: 0,
+
+    overflow: 'hidden',
+  },
+  playerWrapper: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+  },
+  progressBar: {
+    width: '100%',
+    height: 3,
+    backgroundColor: '#6a007a',
+  },
+  progress: {
+    width: '0%',
+    height: '100%',
+    backgroundColor: '#ec73ff',
+  },
+  thumbnailContainer: {
+    width: 60,
+    height: 60,
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  thumbnail: {
+    width: 45,
+    height: 45,
+
+    borderRadius: 5,
+  },
+  aboutContainer: {
+    width: 180,
+    height: 60,
+
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  musicTitle: {
+    width: '100%',
+
+    marginLeft: 5,
+
+    color: 'white',
+
+    fontFamily: 'Lato_700Bold',
+  },
+  musicInfo: {
+    margin: 0,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  musicAuthor: {
+    marginLeft: 5,
+
+    color: 'rgb(180,180,180)',
+
+    fontSize: 13,
+    fontFamily: 'Lato_400Regular',
+  },
+  actions: {
+    flex: 1,
+    height: '100%',
+
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  playIcon: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  pauseIcon: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  loopIcon: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
   },
 });
 
