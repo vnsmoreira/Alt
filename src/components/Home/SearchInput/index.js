1cfcd7aa9fc90315c1941a3bd68fe3cff68fd371
Vinícius <vnsmoreira.me@gmail.com>
2022-02-11 21:04:30 -0300

    Update: add clear input function to search input

--- src/components/Home/SearchInput/index.js
+++ src/components/Home/SearchInput/index.js
@@ -1,9 +1,11 @@
-import { View, TextInput } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 import styles from './styles';
 
 const SearchInput = ({ search, setSearch }) => {
+  const handleClearInput = () => setSearch('');
+
   return (
     <View style={styles.inputContainer}>
       <Ionicons style={styles.searchIcon} name="ios-search-outline" size={25} color="#5c5c5c" />
@@ -16,6 +18,9 @@ const SearchInput = ({ search, setSearch }) => {
         value={search}
         onChangeText={setSearch}
       />
+      <TouchableOpacity onPress={handleClearInput}>
+        <MaterialIcons style={styles.searchIcon} name="clear" size={24} color="#5c5c5c" />
+      </TouchableOpacity>
     </View>
   );
 };
