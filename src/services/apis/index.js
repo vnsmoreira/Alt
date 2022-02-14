711a15a0ac116740fdef2283e62be4a6c431744e
Vinícius <vnsmoreira.me@gmail.com>
2022-02-14 10:21:14 -0300

    Update: export api baseUrl

--- src/services/apis/index.js
+++ src/services/apis/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+export const baseURL = 'http://192.168.15.127:3000';
+
 const api = axios.create({
-  baseURL: 'http://192.168.15.127:3000',
+  baseURL,
   timeout: 6000,
 });
 
