8eadc0559814b9b81d57ee4108f2ff1aa044f4a4
Vinícius <vnsmoreira.me@gmail.com>
2022-02-14 10:05:23 -0300

    Refactor: create api instace and export do endpoints functions

--- src/services/apis/index.js
+++ src/services/apis/index.js
@@ -1,4 +1,11 @@
-export const baseURL = 'http://192.168.15.127:3000';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://192.168.15.127:3000',
+  timeout: 6000,
+});
+
+export default api;
 
 export const downloadEndpoint = '/download/';
 export const searchEndpoint = '/search/';
--- src/services/apis/searchMusics.js
+++ src/services/apis/searchMusics.js
@@ -1,15 +1,11 @@
 import { Alert } from 'react-native';
-import axios from 'axios';
-import { baseURL, searchEndpoint } from './index';
+import api, { searchEndpoint } from './index';
 
 export const getMusics = async query => {
   query = query ? query : 'russ';
 
   try {
-    const url = baseURL + searchEndpoint + query;
-    const response = await axios.get(url, {
-      timeout: 2000,
-    });
+    const response = await api.get(searchEndpoint + query);
 
     return response.data;
   } catch (err) {
