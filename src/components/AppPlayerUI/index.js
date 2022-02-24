25799d8725ebab1bfb4bc0ed9032a181a8446ba3
Vinícius <vnsmoreira.me@gmail.com>
2022-02-24 02:57:06 -0300

    Refactor component to implement trackplayer methods

--- src/components/AppPlayerUI/index.js
+++ src/components/AppPlayerUI/index.js
@@ -1,13 +1,20 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { PlayerContext } from '../../contexts/player';
+import { useProgress } from 'react-native-track-player';
 
 import styles from './styles';
 
 const PlayerUI = () => {
-  const { player, loading, playing, looping, progressPercentage, currentAudioInfo } =
-    useContext(PlayerContext);
+  const { player, playing, looping, currentAudioInfo } = useContext(PlayerContext);
+  const progress = useProgress();
+  const [progressPercentage, setProgressPercentage] = useState(0);
+
+  useEffect(() => {
+    const currentProgress = (progress.position / progress.duration) * 100;
+    setProgressPercentage(currentProgress);
+  }, [progress]);
 
   return (
     <View style={styles.player}>
@@ -16,23 +23,23 @@ const PlayerUI = () => {
       </View>
       <View style={styles.playerWrapper}>
         <View style={styles.thumbnailContainer}>
-          <Image style={styles.thumbnail} source={{ uri: currentAudioInfo.thumbnailUri }} />
+           <Image style={styles.thumbnail} source={{ uri: currentAudioInfo.thumbnailUri }} />
         </View>
         <View style={styles.aboutContainer}>
           <Text numberOfLines={1} style={styles.musicTitle}>
-            {currentAudioInfo.title}
+             {currentAudioInfo.title}
           </Text>
           <View style={styles.musicInfo}>
-            <Text style={styles.musicAuthor}>{currentAudioInfo.author}</Text>
+             <Text style={styles.musicAuthor}>{currentAudioInfo.author}</Text>
           </View>
         </View>
         <View style={styles.actions}>
           {playing ? (
-            <TouchableOpacity style={styles.pauseIcon} onPress={player.pauseAudio}>
+            <TouchableOpacity style={styles.pauseIcon} onPress={player.pause}>
               <Ionicons name="pause" size={24} color="white" />
             </TouchableOpacity>
           ) : (
-            <TouchableOpacity style={styles.playIcon} onPress={player.playAudio}>
+            <TouchableOpacity style={styles.playIcon} onPress={player.play}>
               <FontAwesome name="play" size={24} color="white" />
             </TouchableOpacity>
           )}
