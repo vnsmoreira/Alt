08fb6e3a81b7d8a07afdc369e9bd633b6f9cd521
Vinícius <vnsmoreira.me@gmail.com>
2022-02-27 03:16:59 -0300

    Update: import slider and set track progress as value

--- src/components/PlayerUI/index.js
+++ src/components/PlayerUI/index.js
@@ -1,8 +1,19 @@
 import { Modal, View, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from './styles';
+import { Slider } from '@miblanchard/react-native-slider';
+import { useEffect, useState } from 'react';
+import { useProgress } from 'react-native-track-player';
 
 const PlayerUI = ({ isVisible, toggleModal }) => {
+  const progress = useProgress();
+  const [progressPercentage, setProgressPercentage] = useState(0);
+
+  useEffect(() => {
+    const currentProgress = (progress.position / progress.duration) * 100;
+    setProgressPercentage(currentProgress);
+  }, [progress]);
+
   return (
     <Modal
       onRequestClose={() => toggleModal()}
@@ -16,6 +27,20 @@ const PlayerUI = ({ isVisible, toggleModal }) => {
             <MaterialIcons name="expand-more" size={45} color="gray" style={{ marginLeft: 20 }} />
           </TouchableOpacity>
         </View>
+
+        <Slider
+          containerStyle={{ width: 200 }}
+          value={progressPercentage}
+          minimumValue={0}
+          maximumValue={100}
+          minimumTrackTintColor="#ec73ff"
+          maximumTrackTintColor="#6a007a"
+          thumbStyle={{ width: 10, height: 10 }}
+          thumbTintColor="#ec73ff"
+          onSlidingComplete={([v]) => {
+            console.log('aaa', v);
+          }}
+        />
       </View>
     </Modal>
   );
