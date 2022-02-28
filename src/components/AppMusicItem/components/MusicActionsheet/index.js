b8842ef974e3f33dda9a2caa92b264c8e5c18a0c
Vinícius <vnsmoreira.me@gmail.com>
2022-02-27 22:00:16 -0300

    Update: create MusicActionSheet state and setup props from music

--- src/components/AppMusicItem/components/MusicActionsheet/index.js
+++ src/components/AppMusicItem/components/MusicActionsheet/index.js
@@ -1,19 +1,44 @@
 import { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import ActionSheet from 'react-native-actions-sheet';
+import ActionSheet, { SheetManager } from 'react-native-actions-sheet';
 
 const MusicActionSheet = () => {
   const [musicId, setMusicId] = useState('');
+  const [isDownloaded, setIsDownloaded] = useState(false);
+  const [actions, setActions] = useState({});
+
+  const closeActionSheet = () => SheetManager.hide('music-options');
+
+  const setupActionSheet = ({ id, isDownloaded, actions }) => {
+    setMusicId(id);
+    setIsDownloaded(isDownloaded);
+    setActions(actions);
+  };
+
+  const onClose = () => {
+    setMusicId('');
+    setIsDownloaded(false);
+    setActions({});
+  };
+
+  const handleDeleteAudio = () => {
+    actions.delete && actions.delete();
+
+    closeActionSheet();
+  };
 
   return (
     <ActionSheet
       id="music-options"
       gestureEnabled={true}
       containerStyle={{ backgroundColor: '#141414' }}
-      onBeforeShow={({ id, isDownloaded }) => setMusicId(id)}
+      onBeforeShow={setupActionSheet}
+      onClose={onClose}
     >
       <View style={{ height: 200, backgroundColor: '#141414' }}>
-        <Text style={{ color: 'white' }}>{musicId}</Text>
+        <TouchableOpacity onPress={handleDeleteAudio}>
+          <Text style={{ color: 'white', fontSize: 30 }}>delete audio</Text>
+        </TouchableOpacity>
       </View>
     </ActionSheet>
   );
