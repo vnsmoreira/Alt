a16dd386e60948414340a7b453282c367e58a210
Vinícius <vnsmoreira.me@gmail.com>
2022-02-27 22:41:48 -0300

    Update: create basic style and create handleEditAudio function

--- src/components/AppMusicItem/components/MusicActionsheet/index.js
+++ src/components/AppMusicItem/components/MusicActionsheet/index.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import styles from './styles';
 import { View, Text, TouchableOpacity } from 'react-native';
 import ActionSheet, { SheetManager } from 'react-native-actions-sheet';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 const MusicActionSheet = () => {
   const [musicId, setMusicId] = useState('');
@@ -27,17 +29,28 @@ const MusicActionSheet = () => {
     closeActionSheet();
   };
 
+  const handleEditAudio = () => {
+    actions.edit && actions.edit();
+
+    closeActionSheet();
+  };
+
   return (
     <ActionSheet
       id="music-options"
       gestureEnabled={true}
-      containerStyle={{ backgroundColor: '#141414' }}
+      containerStyle={styles.actionSheetContainer}
       onBeforeShow={setupActionSheet}
       onClose={onClose}
     >
-      <View style={{ height: 200, backgroundColor: '#141414' }}>
-        <TouchableOpacity onPress={handleDeleteAudio}>
-          <Text style={{ color: 'white', fontSize: 30 }}>delete audio</Text>
+      <View style={styles.actionSheetWrapper}>
+        <TouchableOpacity onPress={handleDeleteAudio} style={styles.actionItem}>
+          <MaterialCommunityIcons name="delete" size={24} color="#ec73ff" />
+          <Text style={styles.actionTitle}>delete audio</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleEditAudio} style={styles.actionItem}>
+          <MaterialIcons name="edit" size={24} color="#ec73ff" />
+          <Text style={styles.actionTitle}>edit audio</Text>
         </TouchableOpacity>
       </View>
     </ActionSheet>
--- src/components/AppMusicItem/components/MusicActionsheet/styles.js
+++ src/components/AppMusicItem/components/MusicActionsheet/styles.js
@@ -1,7 +1,32 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
- 
+  actionSheetContainer: {
+    backgroundColor: '#141414',
+  },
+  actionSheetWrapper: {
+    width: '94%',
+    height: 200,
+
+    marginHorizontal: '3%',
+
+    backgroundColor: '#141414',
+  },
+  actionItem: {
+    marginBottom: 20,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  actionTitle: {
+    marginLeft: 10,
+
+    color: 'white',
+    fontSize: 20,
+    fontFamily: 'Lato_400Regular',
+    fontWeight: 'bold',
+    textTransform: 'capitalize',
+  },
 });
 
 export default styles;
