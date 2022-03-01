5f8d64b1b4ca489d88d71c7bbd87065759b9641d
Vinícius <vnsmoreira.me@gmail.com>
2022-03-01 20:19:31 -0300

    Update: create updateMusicItem(callback optional parameter) to automatically update musicList

--- src/components/AppMusicList/index.js
+++ src/components/AppMusicList/index.js
@@ -3,14 +3,32 @@ import MusicItem from '../AppMusicItem';
 
 import styles from './styles';
 
-const MusicList = ({ data }) => {
+const MusicList = ({ data, setData }) => {
+  const updateMusicItem = (id, music) => {
+    setData(data => {
+      const dataRef = data;
+
+      const index = dataRef.findIndex(item => item.id == id);
+      dataRef[index] = music;
+
+      return dataRef;
+    });
+  };
+
   return (
     <FlatList
       onScrollBeginDrag={Keyboard.dismiss}
       contentContainerStyle={styles.musicList}
       data={data}
       keyExtractor={item => item.id}
-      renderItem={({ item, index }) => <MusicItem item={item} index={index} queue={data} />}
+      renderItem={({ item, index }) => (
+        <MusicItem
+          item={item}
+          index={index}
+          queue={data}
+          updateMusicItem={setData ? updateMusicItem : null}
+        />
+      )}
     />
   );
 };
