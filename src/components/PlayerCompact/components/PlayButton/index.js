bd0827e9678a85bb86796a04bb62b5e6ad54abc1
Vinícius <vnsmoreira.me@gmail.com>
2022-03-02 22:41:30 -0300

    Refactor: improve readability by breaking component in separeted functions

--- src/components/PlayerCompact/components/PlayButton/index.js
+++ src/components/PlayerCompact/components/PlayButton/index.js
@@ -3,18 +3,32 @@ import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 import styles from './styles';
 
-const PlayButton = ({ playing, player }) => {
+const pauseButton = (player, disabled, size, color) => (
+  <TouchableOpacity
+    disabled={disabled}
+    style={styles.pauseIcon}
+    onPress={() => player && player.pause()}
+  >
+    <Ionicons name="pause" size={size} color={color} />
+  </TouchableOpacity>
+);
+
+const playButton = (player, disabled, size, color) => (
+  <TouchableOpacity
+    disabled={disabled}
+    style={styles.playIcon}
+    onPress={() => player && player.play()}
+  >
+    <FontAwesome name="play" size={size} color={color} />
+  </TouchableOpacity>
+);
+
+const PlayButton = ({ playing, player, disabled, size = 24, color = 'white' }) => {
   return (
     <View>
-      {playing ? (
-        <TouchableOpacity style={styles.pauseIcon} onPress={player.pause}>
-          <Ionicons name="pause" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity style={styles.playIcon} onPress={player.play}>
-          <FontAwesome name="play" size={24} color="white" />
-        </TouchableOpacity>
-      )}
+      {playing
+        ? pauseButton(player, disabled, size, color)
+        : playButton(player, disabled, size, color)}
     </View>
   );
 };
