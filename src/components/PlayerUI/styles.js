f102b69cd211a853e59a1bb4bd6b07e6adec7b6e
Vinícius <vnsmoreira.me@gmail.com>
2022-03-02 22:45:56 -0300

    Update: create player basic style *to be refactored*

--- src/components/PlayerUI/styles.js
+++ src/components/PlayerUI/styles.js
@@ -1,16 +1,99 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  playerModal: {
-    backgroundColor: '#141414',
-  },
+  playerModal: {},
   playerContainer: {
     width: '100%',
     height: '100%',
+
     backgroundColor: '#141414',
   },
-  playerHeader:{
-  }
+  playerWrapper: {
+    width: '90%',
+    height: '100%',
+
+    marginTop: 20,
+    marginHorizontal: '5%',
+    position: 'relative',
+  },
+  playerHeader: {
+    width: '90%',
+    height: 60,
+
+    marginHorizontal: '5%',
+    marginTop: 10,
+  },
+  playerThumbnails: {
+    width: '100%',
+
+    marginTop: 20,
+    marginTop: '10%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  currentTrackThumbnail: {
+    width: '90%',
+    aspectRatio: 1,
+
+    marginBottom: 40,
+  },
+  aboutContainer: {
+    width: '90%',
+    height: 60,
+
+    marginBottom: 10,
+
+    marginHorizontal: '5%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  musicTitle: {
+    width: '100%',
+
+    color: 'white',
+    fontSize: 25,
+    fontFamily: 'Lato_700Bold',
+  },
+  musicAuthor: {
+    color: 'rgb(180,180,180)',
+
+    fontSize: 18,
+    fontFamily: 'Lato_700Bold',
+  },
+  seekBarContainer: {
+    width: '90%',
+
+    marginHorizontal: '5%',
+
+    alignItems: 'center',
+  },
+  controls: {
+    width: '90%',
+    marginHorizontal: '5%',
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    position: 'absolute',
+    bottom: 35,
+  },
+  mainControls: {
+    flex: 1,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playButtonContainer: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default styles;
