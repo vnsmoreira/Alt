2e50ea0f3160319757160f474eff8bec99b5b27a
Vinícius <vnsmoreira.me@gmail.com>
2022-03-09 07:47:03 -0300

    Create: AppAlbumList component

---
+++ src/components/AppAlbumList/index.js
@@ -0,0 +1,22 @@
+import { FlatList, Keyboard } from 'react-native';
+import AlbumItem from '../AppAlbumItem';
+import styles from './styles';
+
+const AlbumList = ({ data, setData }) => {
+
+  
+  return (
+    <FlatList
+      style={{ height: '100%', width: '100%' }}
+      onScrollBeginDrag={Keyboard.dismiss}
+      contentContainerStyle={{ paddingBottom: 120 }}
+      data={data}
+      keyExtractor={(album, index) => index}
+      renderItem={({ item: album, index }) => (
+        <AlbumItem name={album.name} audioList={album.audioList} />
+      )}
+    />
+  );
+};
+
+export default AlbumList;
