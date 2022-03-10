b31b6ed24daefe1cd165fae4d0076cccb013a435
Vinícius <vnsmoreira.me@gmail.com>
2022-03-10 03:32:07 -0300

    Update: list albums on playlist view

--- src/pages/Playlists/index.js
+++ src/pages/Playlists/index.js
@@ -2,31 +2,58 @@ import { useEffect, useState } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import { Ionicons } from '@expo/vector-icons';
-import MusicList from '../../components/AppMusicList';
+import AlbumList from '../../components/AppAlbumList';
+import CreatePlaylistModal from '../../components/Views/Playlists/CreatePlaylistModal';
 import * as realm from '../../services/realm';
 
 export default function Playlists() {
-  const [audios, setAudios] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [modalVisibility, setModalVisibility] = useState(false);
 
-  async function updateAudios() {
-    const audios = await realm.getAudioCollection();
-    setAudios(audios);
+  async function setupAlbums() {
+    const albumsArray = await realm.getAlbumCollection();
+    setAlbums(albumsArray);
   }
 
   useEffect(() => {
-    realm.onAudioCollectionUpdate(updateAudios);
+    realm.addCollectionListener('Album', setupAlbums);
   }, []);
 
   return (
     <View style={styles.playlists}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerTitle}>Suas Playlists</Text>
-        <TouchableOpacity onPress={updateAudios} style={{ padding: 5 }}>
-          <Ionicons name="ios-reload-circle" size={30} color="#ec73ff" />
-        </TouchableOpacity>
+      <View style={[styles.headerContainer, { flexDirection: 'column' }]}>
+        <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={styles.headerTitle}>Suas Playlists</Text>
+          <TouchableOpacity onPress={setupAlbums} style={{ padding: 5, paddingRight: 0 }}>
+            <Ionicons name="ios-reload-circle" size={30} color="#ec73ff" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity
+            onPress={() => setModalVisibility(true)}
+            style={{ backgroundColor: 'gray', padding: 5 }}
+          >
+            <Text
+              style={{
+                color: 'black',
+                fontFamily: 'Lato_700Bold',
+                fontWeight: 'bold',
+                textTransform: 'capitalize',
+              }}
+            >
+              criar playlist
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <MusicList data={audios} />
+      <AlbumList data={albums} />
+
+      <CreatePlaylistModal
+        modalVisibility={modalVisibility}
+        setModalVisibility={setModalVisibility}
+      />
     </View>
   );
 }
--- src/pages/Playlists/styles.js
+++ src/pages/Playlists/styles.js
@@ -3,18 +3,22 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   playlists: {
     width: '100%',
-    flex: 1,
 
     backgroundColor: '#141414',
+
+    flex: 1,
+    alignItems: 'flex-start',
   },
   headerContainer: {
     width: '94%',
+
     marginTop: 70,
     marginHorizontal: '3%',
     marginBottom: 40,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between'
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   headerTitle: {
     marginBottom: 10,
@@ -25,8 +29,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Lato_700Bold',
     fontWeight: 'bold',
     textTransform: 'capitalize',
-
-
   },
 });
 
