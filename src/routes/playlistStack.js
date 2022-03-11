ba4d27e175df7b22bcdcfdf8b8fa710e2451e4d1
Vinícius <vnsmoreira.me@gmail.com>
2022-03-11 01:29:41 -0300

    Update: add Playlist route

--- src/routes/playlistStack.js
+++ src/routes/playlistStack.js
@@ -1,13 +1,18 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Playlists from '../pages/Playlists';
+import Playlist from '../pages/Playlist';
 
 const Stack = createStackNavigator();
 
 export default function PlaylistStack() {
   return (
-    <Stack.Navigator initialRouteName="Playlists" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Playlists"
+      screenOptions={{ headerShown: false, animationEnabled: false }}
+    >
       <Stack.Screen name="Playlists" component={Playlists} />
+      <Stack.Screen name="Playlist" component={Playlist} />
     </Stack.Navigator>
   );
 }
