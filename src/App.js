76ab9e89ba7ad755b06db1d3927204bd11be1cc6
Vinícius <vnsmoreira.me@gmail.com>
2022-03-13 17:55:59 -0300

    Update: remove player components useless props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
+import { useFonts, Lato_300Light, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+
+/* contexts */
+import { PlayerProvider } from './contexts/player';
+
+/* components */
 import { NavigationContainer } from '@react-navigation/native';
 import AppRoutes from './routes';
 import AppLoading from 'expo-app-loading';
-import { useFonts, Lato_300Light, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
-import { PlayerProvider } from './contexts/player';
-import { useState } from 'react';
+
 import PlayerCompact from './components/PlayerUI/PlayerCompact';
 import PlayerModal from './components/PlayerUI/PlayerModal';
 import AppActionSheet from './components/AppActionSheet';
 
 export default function App() {
-  const [showModal, setShowModal] = useState(false);
-  const handleToggleModal = () => setShowModal(!showModal);
-
   let [fontsLoaded] = useFonts({
     Lato_300Light,
     Lato_400Regular,
@@ -28,8 +29,8 @@ export default function App() {
         <NavigationContainer>
           <StatusBar translucent backgroundColor="transparent" />
           <AppRoutes />
-          <PlayerCompact toggleModal={handleToggleModal} />
-          <PlayerModal isVisible={showModal} toggleModal={handleToggleModal} />
+          <PlayerCompact />
+          <PlayerModal />
           <AppActionSheet />
         </NavigationContainer>
       </PlayerProvider>
